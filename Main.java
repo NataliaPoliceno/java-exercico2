4661cff76ee6ef214a2df6f622abc681623dbbab
natalia policeno <policenorosa17@gmail.com>
2022-05-07 13:57:18 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("exercicio java");
+        Quadrilatero.area(4);
+        Quadrilatero.area(6,3);
+        Quadrilatero.area(3,6,9);
+        Quadrilatero.area(6f,6f);
+    }
+}
